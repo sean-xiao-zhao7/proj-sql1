5433c4d104b5cd95c92e8500308c5b1f30851f6c
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-04-26 16:59:14 -0400

    Alter volume to NUMERIC(10,3)

--- sales.sql
+++ sales.sql
@@ -1,12 +1,15 @@
 USE sales;
-CREATE TABLE sales (
+CREATE TABLE IF NOT EXISTS sales (
     id INT AUTO_INCREMENT,
     date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     date_fulfilled TIMESTAMP,
     customer_name VARCHAR(250) NOT NULL,
     product_name VARCHAR(250) NOT NULL,
-    volume INT,
+    volume NUMERIC(10,3) NOT NULL CHECK (volume >= 0),
     is_recurring BOOLEAN DEFAULT FALSE,
     is_disputed BOOLEAN DEFAULT FALSE,
     PRIMARY KEY (id)
-)
+);
+
+-- ALTER TABLE sales
+-- MODIFY volume NUMERIC(10,3) NOT NULL CHECK (volume >= 0);
