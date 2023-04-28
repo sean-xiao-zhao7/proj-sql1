81017eacf346e7af7b16b6a7d846432cae254dad
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-04-28 13:23:51 -0400

    Fix Company Z values

---
+++ sales_updates.sql
@@ -0,0 +1,17 @@
+-- INSERT INTO sales (
+--         date_created,
+--         date_fulfilled,
+--         customer_name,
+--         product_name,
+--         volume
+--     )
+-- VALUES (
+--         '2021-12-09',
+--         '2021-12-17',
+--         'Company Z',
+--         'A Tuck',
+--         109.0
+--     );
+-- update sales
+-- set volume = volume * 1000
+-- where customer_name = 'Company Z';
