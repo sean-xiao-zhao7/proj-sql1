301281513d113a0e612010c5b27f343da1247ece
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-05-05 12:58:33 -0400

    Use FLOOR

--- group_having.sql
+++ group_having.sql
@@ -1,10 +1,6 @@
 use restaurants;
-select DATE_FORMAT(
-        FROM_UNIXTIME(`booking_date`),
-        '%Y-%m-%d %H:%i:%s'
-    ) as date,
-    COUNT(booking_date) as num_bookings,
-    SUM(billed) as sum
+select COUNT(booking_date) as num_bookings,
+    FLOOR(SUM(billed)) as sum
 FROM bookings
 GROUP BY booking_date
 HAVING SUM(billed) > 30
