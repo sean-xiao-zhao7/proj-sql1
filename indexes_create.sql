59da76d136775231b97cbfd773e36143860c557c
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-05-08 14:16:12 -0400

    Try to use partial index

--- indexes_create.sql
+++ indexes_create.sql
@@ -4,5 +4,5 @@ SELECT *
 from addresses
 where street like 'Test*'
     and city = 'Munich';
--- CREATE UNIQUE INDEX salary_idx ON users (salary);
+-- CREATE INDEX salary_idx ON users (salary) WHERE salary > 12000; -- NOT SUPPORTED IN MYSQL
 -- CREATE INDEX addresses_idx_multi ON addresses (street, city);
