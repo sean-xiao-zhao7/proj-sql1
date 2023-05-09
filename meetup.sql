85dff7fedb93197a50198eb457e838c7df4f4ab3
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-05-09 12:40:32 -0400

    Fix event/location syntax errors

--- meetup.sql
+++ meetup.sql
@@ -1,7 +1,7 @@
 -- CREATE DATABASE IF NOT EXISTS meetup;
 use meetup;
 CREATE TABLE IF NOT EXISTS location (
-    id AUTO_INCREMENT,
+    id INT AUTO_INCREMENT,
     city VARCHAR(250) NOT NULL,
     state VARCHAR(250),
     country VARCHAR(250),
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS location (
     PRIMARY KEY (id)
 );
 CREATE TABLE IF NOT EXISTS event (
-    id AUTO_INCREMENT,
+    id INT AUTO_INCREMENT,
     event_name VARCHAR(250) NOT NULL,
     event_image_url VARCHAR(500),
     event_start_datetime DATETIME DEFAULT NOW(),
@@ -18,5 +18,7 @@ CREATE TABLE IF NOT EXISTS event (
     event_max_members INT DEFAULT 0,
     event_age_rating VARCHAR(250),
     event_location_id INT,
-    PRIMARY KEY (id)
+    CONSTRAINT event_location_fkey FOREIGN KEY (event_location_id) REFERENCES location(id) ON DELETE
+    SET NULL,
+        PRIMARY KEY (id)
 );
