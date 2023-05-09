b67f8de9b2c94a94457af86b424ab25a5c6e488e
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-05-09 12:54:26 -0400

    Add user table

--- meetup.sql
+++ meetup.sql
@@ -1,10 +1,31 @@
 -- CREATE DATABASE IF NOT EXISTS meetup;
 use meetup;
+-- many to many, user to event
+CREATE TABLE IF NOT EXISTS user_event (
+    id INT AUTO_INCREMENT,
+    user_id INT,
+    event_id INT,
+    CONSTRAINT user_event_user_fkey FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE,
+    CONSTRAINT user_event_event_fkey FOREIGN KEY (event_id) REFERENCES event(id) ON DELETE
+    SET NULL,
+        PRIMARY KEY (id)
+);
+-- user account
+CREATE TABLE IF NOT EXISTS user (
+    id INT AUTO_INCREMENT,
+    first_name VARCHAR(250) NOT NULL,
+    last_name VARCHAR(250) NOT NULL,
+    email VARCHAR(250) NOT NULL,
+    type VARCHAR(250) DEFAULT 'normal_user',
+    PRIMARY KEY (id)
+);
+-- list of events
 CREATE TABLE IF NOT EXISTS event_category (
     id INT AUTO_INCREMENT,
     event_category_name VARCHAR(250) NOT NULL,
     PRIMARY KEY (id)
 );
+-- single location/address, belong to many events
 CREATE TABLE IF NOT EXISTS location (
     id INT AUTO_INCREMENT,
     city VARCHAR(250) NOT NULL,
@@ -14,6 +35,7 @@ CREATE TABLE IF NOT EXISTS location (
     address VARCHAR(250),
     PRIMARY KEY (id)
 );
+-- single event
 CREATE TABLE IF NOT EXISTS event (
     id INT AUTO_INCREMENT,
     event_name VARCHAR(250) NOT NULL,
