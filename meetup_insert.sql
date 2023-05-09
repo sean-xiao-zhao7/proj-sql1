4248e8f2c45c712e3adf26eb1be7da8bd42b6aaf
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-05-09 13:09:18 -0400

    Insert city data

---
+++ meetup_insert.sql
@@ -0,0 +1,8 @@
+use meetup;
+INSERT INTO location (city, state, country, postal)
+VALUES ('City 1', 'State 1', 'Country 1', 'Postal 1'),
+    ('City 2', 'State 2', 'Country 2', 'Postal 2'),
+    ('City 3', 'State 3', 'Country 3', 'Postal 3'),
+    ('City 4', 'State 4', 'Country 4', 'Postal 4'),
+    ('City 5', 'State 5', 'Country 5', 'Postal 5'),
+    ('City 6', 'State 6', 'Country 6', 'Postal 6');
