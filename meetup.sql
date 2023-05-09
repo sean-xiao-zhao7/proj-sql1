818ae8da73f119ea9e8475347f5a7d89e06be6eb
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-05-09 12:36:45 -0400

    Add event table

--- meetup.sql
+++ meetup.sql
@@ -1,1 +1,13 @@
-CREATE DATABASE IF NOT EXISTS meetup;
+-- CREATE DATABASE IF NOT EXISTS meetup;
+use meetup;
+CREATE TABLE if NOT EXISTS event (
+    id AUTO_INCREMENT,
+    event_name VARCHAR(250) NOT NULL,
+    event_image_url VARCHAR(500),
+    event_start_datetime DATETIME DEFAULT NOW(),
+    event_description TEXT,
+    event_max_members INT DEFAULT 0,
+    event_age_rating VARCHAR(250),
+    event_location_id INT,
+    PRIMARY KEY (id)
+);
