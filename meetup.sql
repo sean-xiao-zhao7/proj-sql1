976e1bcf0090a6bc512f960fe6b44cde1a65a31a
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-05-09 13:15:26 -0400

    Insert into event_category

--- meetup.sql
+++ meetup.sql
@@ -1,5 +1,29 @@
 -- CREATE DATABASE IF NOT EXISTS meetup;
 use meetup;
+-- list of events
+CREATE TABLE IF NOT EXISTS event_category (
+    id INT AUTO_INCREMENT,
+    event_category_name VARCHAR(250) NOT NULL,
+    event_category_description TEXT,
+    PRIMARY KEY (id)
+);
+-- single event
+CREATE TABLE IF NOT EXISTS event (
+    id INT AUTO_INCREMENT,
+    event_name VARCHAR(250) NOT NULL,
+    event_image_url VARCHAR(500),
+    event_start_datetime DATETIME DEFAULT NOW(),
+    event_description TEXT,
+    event_max_members INT DEFAULT 0,
+    event_age_rating VARCHAR(250),
+    event_location_id INT,
+    event_category_id INT,
+    CONSTRAINT event_location_fkey FOREIGN KEY (event_location_id) REFERENCES location(id) ON DELETE
+    SET NULL,
+        CONSTRAINT event_category_fkey FOREIGN KEY (event_category_id) REFERENCES event_category(id) ON DELETE
+    SET NULL,
+        PRIMARY KEY (id)
+);
 -- many to many, user to event
 CREATE TABLE IF NOT EXISTS user_event (
     id INT AUTO_INCREMENT,
@@ -19,13 +43,6 @@ CREATE TABLE IF NOT EXISTS user (
     type VARCHAR(250) DEFAULT 'normal_user',
     PRIMARY KEY (id)
 );
--- list of events
-CREATE TABLE IF NOT EXISTS event_category (
-    id INT AUTO_INCREMENT,
-    event_category_name VARCHAR(250) NOT NULL,
-    event_category_description TEXT,
-    PRIMARY KEY (id)
-);
 -- single location/address, belong to many events
 CREATE TABLE IF NOT EXISTS location (
     id INT AUTO_INCREMENT,
@@ -35,21 +52,4 @@ CREATE TABLE IF NOT EXISTS location (
     postal VARCHAR(250),
     address VARCHAR(250),
     PRIMARY KEY (id)
-);
--- single event
-CREATE TABLE IF NOT EXISTS event (
-    id INT AUTO_INCREMENT,
-    event_name VARCHAR(250) NOT NULL,
-    event_image_url VARCHAR(500),
-    event_start_datetime DATETIME DEFAULT NOW(),
-    event_description TEXT,
-    event_max_members INT DEFAULT 0,
-    event_age_rating VARCHAR(250),
-    event_location_id INT,
-    event_category_id INT,
-    CONSTRAINT event_location_fkey FOREIGN KEY (event_location_id) REFERENCES location(id) ON DELETE
-    SET NULL,
-        CONSTRAINT event_category_fkey FOREIGN KEY (event_category_id) REFERENCES event_category(id) ON DELETE
-    SET NULL,
-        PRIMARY KEY (id)
 );
--- meetup_insert.sql
+++ meetup_insert.sql
@@ -1,8 +1,13 @@
 use meetup;
-INSERT INTO location (city, state, country, postal)
-VALUES ('City 1', 'State 1', 'Country 1', 'Postal 1'),
-    ('City 2', 'State 2', 'Country 2', 'Postal 2'),
-    ('City 3', 'State 3', 'Country 3', 'Postal 3'),
-    ('City 4', 'State 4', 'Country 4', 'Postal 4'),
-    ('City 5', 'State 5', 'Country 5', 'Postal 5'),
-    ('City 6', 'State 6', 'Country 6', 'Postal 6');
+-- INSERT INTO location (city, state, country, postal)
+-- VALUES ('City 1', 'State 1', 'Country 1', 'Postal 1'),
+--     ('City 2', 'State 2', 'Country 2', 'Postal 2'),
+--     ('City 3', 'State 3', 'Country 3', 'Postal 3'),
+--     ('City 4', 'State 4', 'Country 4', 'Postal 4'),
+--     ('City 5', 'State 5', 'Country 5', 'Postal 5'),
+--     ('City 6', 'State 6', 'Country 6', 'Postal 6');
+INSERT INTO event_category (event_category_name, event_category_description)
+VALUES (
+        'Event Category 1',
+        'Event Category Description.'
+    );
